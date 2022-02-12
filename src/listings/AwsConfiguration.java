59bdf817888896eae4da26e530f52491b4379ced
Joshua Villing <joshua.villing@mimacom.com>
2022-02-12 15:15:47 +0100

    Work on t2s concept

--- src/listings/AwsConfiguration.java
+++ src/listings/AwsConfiguration.java
@@ -6,23 +6,15 @@ public class AwsConfiguration {
 
     @Bean
     public AmazonPollyClient amazonPollyClient() {
-        var credentialsProvider = credentialsProvider();
-        var clientConfiguration = new ClientConfiguration();
-        var polly = new AmazonPollyClient(credentialsProvider, clientConfiguration);
-        polly.setRegion(region);
-        return polly;
+        ...
     }
 
     @Bean
     public Voice voice(AmazonPollyClient polly) {
-        var describeVoicesRequest = new DescribeVoicesRequest();
-        var describeVoicesResult = polly.describeVoices(describeVoicesRequest);
-        return describeVoicesResult.getVoices().stream()
-               .filter(v -> v.getLanguageName().equals(language)).findFirst().get();
+        ...
     }
 
     private AWSStaticCredentialsProvider credentialsProvider() {
-        var basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
-        return new AWSStaticCredentialsProvider(basicAWSCredentials);
+        ...
     }
 }
--- src/listings/PraxisrufApi+Speech.swift
+++ src/listings/PraxisrufApi+Speech.swift
@@ -1,17 +1,5 @@
 extension PraxisrufApi {
-    func synthesize(authToken: String, completion: @escaping (Result<URL, PraxisrufApiError>) -> Void) {
-        guard let audioUrl = URL(string: "\(baseUrlValue)/speechsynthesis") else {
-            completion(.failure(.custom(errorMessage: "Invalid url configuration")))
-            return
-        }
-        var request = URLRequest(url: audioUrl)
-        request.addValue("Bearer \(authToken)", forHTTPHeaderField: "Authorization")
-        URLSession.shared.downloadTask(with: request) { result, response, error in
-            guard let audioFileLocation = result else {
-                completion(.failure(.custom(errorMessage: "No audio received")))
-                return
-            }
-            completion(.success(audioFileLocation))
-        }.resume()
-    }
+       func download(_ subUrl: String, completion: @escaping CompletionHandler<T>) {
+        ...
+       }
 }
