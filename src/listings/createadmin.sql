1893e01294b06147bf2bc5e1db479eb233d04701
Joshua Villing <joshua.villing@mimacom.com>
2022-03-20 10:15:21 +0100

    Add installation manual

---
+++ src/listings/createadmin.sql
@@ -0,0 +1,4 @@
+insert into
+    praxis_intercom_user (id, name, password, role, user_name)
+values
+    (<uuid>, 'admin', <encoded password>, 'admin', <username>);
---
+++ src/listings/missedcallnotification.sql
@@ -0,0 +1,5 @@
+INSERT INTO
+    notification_type (id, body, display_text, text_to_speech, title, type, version)
+VALUES
+    ('63d530ab-48af-4597-a9fd-2fb4c9700c55', 'Verpasster Anruf',
+    'Verpasster Anruf', false, 'Verpasster Anruf', 'Verpasster Anruf', 0);
